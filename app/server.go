d1eea106549bee9c06a9d228c409f9f00aab4b9e
Abas Farah <abasnfarah@gmail.com>
2023-12-25 12:30:27 -0600

    Creates connection to TCP server

--- app/server.go
+++ app/server.go
@@ -19,9 +19,20 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+  c, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+  defer c.Close()
+
+  d, err := c.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+  if err != nil {
+    fmt.Println("Error writing to connection: ", err.Error())
+    os.Exit(1)
+  }
+
+  fmt.Printf("Received message: %d\n", d)
+
 }
